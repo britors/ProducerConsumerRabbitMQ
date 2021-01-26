a25146cf466db287ef506ccf66d7633b9bd32dab
Rodrigo Brito <rodrigo@w3ti.com.br>
2021-01-26 12:43:36 -0300

    Add project files.

---
+++ Consumers/Consumer/Receiver.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+
+namespace Consumer
+{
+    /// <summary>
+    /// Basic Consumer from RabbitMQ
+    /// </summary>
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            // create a factory
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            // create a connection from factory
+            using (var connection = factory.CreateConnection())
+                // create a channel
+            using (var chanel = connection.CreateModel())
+            {
+                // declare the Queue
+                chanel.QueueDeclare("BasicTest", false, false, false, null);
+                // declare then Event
+                var consumer = new EventingBasicConsumer(chanel);
+                // Received from Event
+                consumer.Received += (model, ea) =>
+                {
+                    // get message body
+                    var body = ea.Body.ToArray();
+                    // convert array to string
+                    var message = Encoding.UTF8.GetString(body);
+                    // write the message in console
+                    Console.WriteLine("Recived message {0}", message);
+                };
+                // Cosume
+                chanel.BasicConsume("BasicTest", true, consumer);
+            }
+            // exit
+            Console.WriteLine("Press [enter] to exit consumer");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Producers/Producer/Sender.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+/// <summary>
+/// Create a very basic RabbitMQ's Producer
+/// </summary>
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            // Create a factory to my rabbitmq
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            // Create a connection
+            using var connection = factory.CreateConnection();
+            //Create a chanel
+            using (var chanel = connection.CreateModel())
+            {
+                // Declare a Queue
+                chanel.QueueDeclare("BasicTest", false, false, false, null);
+                // Declare the message
+                string message = "Getting startup with .Net Core RabbitMQ";
+                // Body is UTF8 Byte array from the string
+                var body = Encoding.UTF8.GetBytes(message);
+                // Send (publish) the body in Queue BasicTest
+                chanel.BasicPublish("", "BasicTest", null, body);
+                // Informe message Sent
+                Console.WriteLine("Sent message {0}", message);
+            }
+
+            Console.WriteLine("Press [enter] to exit then Sender.");
+            Console.ReadLine();
+        }
+    }
+}
